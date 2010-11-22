0b1370fa9ec8e2c0e0f58bee99b9cc2f437b1987
Nirav Shah <niravhgsc@gmail.com>
2010-11-22 14:40:52 -0600

    Zip sequence files during BWA postrun and delete .sam and .sai files

--- bin/bwa_postrun.rb
+++ bin/bwa_postrun.rb
@@ -13,3 +13,15 @@ puts output
 
 # Appropriate code can be added here to clean up the GERALD directory of
 # unwanted files such as .sam, intermediate .bam files, .sai files etc.
+
+puts "Deleting Temp Files"
+deleteTempFilesCmd = "rm *.sam *.sai"
+`#{deleteTempFilesCmd}`
+
+puts "Zipping sequence files"
+zipCommand = "bzip2 *sequence.txt"
+s = Scheduler.new("Zipping_seq_files", zipCommand)
+s.setMemory(1000)
+s.setNodeCores(1)
+s.setPriority("high")
+s.runCommand()
