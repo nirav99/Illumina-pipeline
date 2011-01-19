7fb46495dbd1c6d3767281cffe56772d4d2390d6
Nirav Shah <niravhgsc@gmail.com>
2011-01-19 14:12:42 -0600

    Change recepient of email messages

--- bin/email_analysis_summary.sh
+++ bin/email_analysis_summary.sh
@@ -12,7 +12,7 @@ attachFile="Summary.htm"
 # pwd -P commands prints the actual path, not the linked path
 bodyText="The path to the Summary.htm file is : "`pwd -P`
 #emailDest="niravs@bcm.edu"
-emailDest="jgreid@bcm.edu niravs@bcm.edu yhan@bcm.edu fongeri@bcm.edu javaid@bcm.edu dc12@bcm.edu english@bcm.edu"
+emailDest="jgreid@bcm.edu niravs@bcm.edu yhan@bcm.edu fongeri@bcm.edu javaid@bcm.edu dc12@bcm.edu pc2@bcm.edu"
 emailSub="Slx Analysis Result Summary"
 
 java -jar /stornext/snfs5/next-gen/Illumina/ipipe/java/SlxResultSummaryMailer.jar \
