b816cf4cae97c25bc4d6cd5c37f322303c3686b4
Nirav Shah <niravhgsc@gmail.com>
2011-01-19 16:19:15 -0600

    A class to encapsulate email functionality in the pipeline

---
+++ lib/EmailHelper.rb
@@ -0,0 +1,78 @@
+#!/usr/bin/ruby
+require 'rubygems'
+require 'hpricot'
+require 'fileutils'
+require 'net/smtp'
+
+# This class encapsulates common routines required by email utilities
+class EmailHelper
+  def initialize()
+    @resultRecepients = nil
+    @errorRecepients  = nil
+    getEmailList()
+  end
+
+  # Get a list of recepients to receive result email
+  def getResultRecepientEmailList()
+    return @resultRecepients
+  end
+
+  # Get a list of recepients to receive error email
+  def getErrorRecepientEmailList()
+    return @errorRecepients
+  end  
+
+  # Method to send an email
+  # Parameter "to" is an array of email addresses 
+  def sendEmail(from, to, subject, message)
+     toMail = ""
+     to.each { |x| toMail= toMail + ",#{x}" }
+
+msg = <<END_OF_MESSAGE
+From: <#{from}>
+To: <#{toMail}>
+Subject: #{subject}
+
+#{message}
+END_OF_MESSAGE
+
+      Net::SMTP.start('smtp.bcm.tmc.edu') do |smtp|
+      smtp.send_message msg, from, to
+    end
+  end
+
+  # Method to send email with a single attachment. Parameter "to" is an array
+  # of recepients
+  def sendEmailWithAttachment(from, to, subject, message, attachment)
+    jarPath = File.dirname(File.dirname(__FILE__)) + "/java/SlxResultSummaryMailer.jar"
+   
+    recepients = to.join(" ")
+ 
+    cmd = "java -jar " + jarPath + " " + from + " \"" + subject + "\" " + attachment +
+          " \"" + message + "\" " + recepients 
+
+    puts cmd
+    `#{cmd}`
+  end
+ 
+  private
+  # Obtain the list of email addresses who need to be emailed the results
+  # or errors
+  def getEmailList()
+    emailListFile = File.dirname(File.dirname(__FILE__)) + "/config/" +
+                    "email_recepients.txt"
+    lines = IO.readlines(emailListFile)
+
+    lines.each do |line|
+      if line.match(/^EMAIL_RESULTS/)
+        temp = line.gsub(/EMAIL_RESULTS=/, "")
+        temp.strip!
+        @resultRecepients = temp.split(",")
+      elsif line.match(/^EMAIL_ERRORS/)
+        temp = line.gsub(/EMAIL_ERRORS=/, "")
+        temp.strip!
+        @errorRecepients = temp.split(",")        
+      end
+    end
+  end
+end
