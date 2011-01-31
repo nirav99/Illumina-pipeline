05d5497117f4c1edb33c73d105f19b6181c4a976
Nirav Shah <niravhgsc@gmail.com>
2011-01-31 14:46:01 -0600

    Changed name of few constants for LIMS upload. Removed parentheses around the value for average coverage

--- bin/CaptureStats.rb
+++ bin/CaptureStats.rb
@@ -134,7 +134,20 @@ class CaptureStatsResults
 
     lines.each do |line|
       if line.index(":") != nil
-        if line.match(/Aligned Reads On-Buffer/)
+        if line.match(/Total Reads Produced/)
+           tokens = line.split(",")
+           @totalReadsProduced = tokens[1].strip
+        elsif line.match(/Duplicate Reads/)
+           tokens = line.split(",")
+           @numDuplicateReads = tokens[1].strip
+           @perDuplicateReads = formatPercentageValue(tokens[2].strip)
+        elsif line.match(/Total Reads Aligned/)
+           tokens = line.split(",")
+           @numAlignedReads      = tokens[1].strip
+           @perAlignedReads      = formatPercentageValue(tokens[2].strip)
+           @numReadsPaired       = tokens[4].strip
+           @numReadAndMatePaired = tokens[6].strip
+        elsif line.match(/Aligned Reads On-Buffer/)
            tokens = line.split(",")
            @numBufferAlignedReads = tokens[1].strip
            @perBufferAlignedReads = formatPercentageValue(tokens[2].strip)
@@ -142,6 +155,19 @@ class CaptureStatsResults
           tokens = line.split(",")
           @numTargetAlignedReads = tokens[1].strip
           @perTargetAlignedReads = formatPercentageValue(tokens[2].strip)
+        elsif line.match(/Average Coverage/)
+          tokens = line.split(",")
+          @avgCoverage = removeParentheses(tokens[2].strip)
+        elsif line.match(/Reads that hit target or buffer/)
+          tokens = line.split(",")
+          @numReadsTargetBuffer = tokens[1].strip
+          @perReadsTargetBuffer = formatPercentageValue(tokens[2].strip)
+        elsif line.match(/Total Aligned Reads/) && line.match(/expected/)
+          tokens = line.split(",")
+          @totalExpAlignedReads = tokens[1].strip
+        elsif line.match(/Total Aligned Reads/) && line.match(/calculated/)
+          tokens = line.split(",")
+          @totalCalcAlignedReads = tokens[1].strip
         elsif line.match(/Targets Hit/)
           tokens = line.split(",")
           @numTargetsHit = tokens[1].strip
@@ -191,24 +217,34 @@ class CaptureStatsResults
 
   # Return the string in a format suitable for display on screen / email
   def formatForSTDOUT()
-    output =  "Buffer aligned reads = " + @numBufferAlignedReads.to_s + " (" + @perBufferAlignedReads.to_s + ")%\r\n" + 
-     "Target aligned reads = " + @numTargetAlignedReads.to_s + " (" + @perTargetAlignedReads.to_s + ")%\r\n" +
-     "Targets hit = " + @numTargetsHit.to_s + " (" + @perTargetsHit.to_s + ")%\r\n" +
-     "Target buffers hit = " + @numTargetBuffersHit.to_s + " (" + @perTargetBuffersHit.to_s + ")%\r\n" +
-     "Total targets = " + @numTotalTargets.to_s + "\r\n" +
-     "Non target hits = " + @numNonTarget.to_s + "\r\n" +
-     "Bases on target = " + @numTargetedBases.to_s + "\r\n" +
-     "Buffer bases = " + @numBufferBases.to_s + "\r\n" + 
-     "Bases with 1+ coverage = " + @numBases1Coverage.to_s + " (" + @perBases1Coverage.to_s + ")%\r\n" +
-     "Bases with 4+ coverage = " + @numBases4Coverage.to_s + " (" + @perBases4Coverage.to_s + ")%\r\n" +
-     "Bases with 10+ coverage = " + @numBases10Coverage.to_s + " (" + @perBases10Coverage.to_s + ")%\r\n" +
-     "Bases with 20+ coverage = " + @numBases20Coverage.to_s + " (" + @perBases20Coverage.to_s + ")%\r\n" +
-     "Bases with 40+ coverage = " + @numBases40Coverage.to_s + " (" + @perBases40Coverage.to_s + ")%\r\n"
+    output = "Total Reads Produced = " + @totalReadsProduced.to_s + "\r\n" +
+     "Duplicate Reads = " + @numDuplicateReads.to_s + " (" + @perDuplicateReads.to_s + ")%\r\n" +
+     "Total Reads Aligned = " + @numAlignedReads.to_s + " (" + @perAlignedReads.to_s + ")%\r\n" +
+     "Paired Reads = " + @numReadsPaired.to_s + "\r\n" +
+     "Reads Paired with Mapped Mates = " + @numReadAndMatePaired.to_s + "\r\n" +
+     "Buffer Aligned Reads = " + @numBufferAlignedReads.to_s + " (" + @perBufferAlignedReads.to_s + ")%\r\n" + 
+     "Target Aligned Reads = " + @numTargetAlignedReads.to_s + " (" + @perTargetAlignedReads.to_s + ")%\r\n" +
+     "Average Coverage = " + @avgCoverage.to_s + "\r\n" +
+     "Reads on Target or Buffer = " + @numReadsTargetBuffer.to_s + " (" + @perReadsTargetBuffer.to_s +  ")%\r\n" +
+     "Total Aligned Reads (Expected) = " + @totalExpAlignedReads.to_s + "\r\n" +
+     "Total Aligned Reads (Calculated) = " + @totalCalcAlignedReads.to_s + "\r\n" +
+     "Targets Hit = " + @numTargetsHit.to_s + " (" + @perTargetsHit.to_s + ")%\r\n" +
+     "Target Buffers Hit = " + @numTargetBuffersHit.to_s + " (" + @perTargetBuffersHit.to_s + ")%\r\n" +
+     "Total Targets = " + @numTotalTargets.to_s + "\r\n" +
+     "Non Target Hits = " + @numNonTarget.to_s + "\r\n" +
+     "Bases on Target = " + @numTargetedBases.to_s + "\r\n" +
+     "Buffer Bases = " + @numBufferBases.to_s + "\r\n" + 
+     "Bases with 1+ Coverage = " + @numBases1Coverage.to_s + " (" + @perBases1Coverage.to_s + ")%\r\n" +
+     "Bases with 4+ Coverage = " + @numBases4Coverage.to_s + " (" + @perBases4Coverage.to_s + ")%\r\n" +
+     "Bases with 10+ Coverage = " + @numBases10Coverage.to_s + " (" + @perBases10Coverage.to_s + ")%\r\n" +
+     "Bases with 20+ Coverage = " + @numBases20Coverage.to_s + " (" + @perBases20Coverage.to_s + ")%\r\n" +
+     "Bases with 40+ Coverage = " + @numBases40Coverage.to_s + " (" + @perBases40Coverage.to_s + ")%\r\n"
      return output
   end
 
+=begin
   # Method to create a result string to upload to LIMS
-  def formatForLIMSUpload()
+  def formatForLIMSUpload_old()
     result = "BUFFER_ALIGNED_READS " + @numBufferAlignedReads.to_s +
     " PERCENT_BUFFER_ALIGNED_READS " + @perBufferAlignedReads.to_s +
     " TARGET_ALIGNED_READS " + @numTargetAlignedReads.to_s +
@@ -219,26 +255,76 @@ class CaptureStatsResults
     " TOTAL_TARGETS " + @numTotalTargets.to_s +
     " HIGH_COVERAGE_NON_TARGET_HITS " + @numNonTarget.to_s +
     " BASES_ON_TARGET " + @numTargetedBases.to_s + " BASES_ON_BUFFER " + @numBufferBases.to_s +
-    " 1_COVERAGE_BASES " + @numBases1Coverage.to_s + 
-    " PER_1_COVERAGE_BASES " + @perBases1Coverage.to_s +
-    " 4_COVERAGE_BASES " + @numBases4Coverage.to_s + 
-    " PER_4_COVERAGE_BASES " + @perBases4Coverage.to_s +
-    " 10_COVERAGE_BASES " + @numBases10Coverage.to_s + 
-    " PER_10_COVERAGE_BASES " + @perBases10Coverage.to_s +
-    " 20_COVERAGE_BASES " + @numBases20Coverage.to_s + 
-    " PER_20_COVERAGE_BASES " + @perBases20Coverage.to_s +
-    " 40_COVERAGE_BASES " + @numBases40Coverage.to_s + 
-    " PER_40_COVERAGE_BASES " + @perBases40Coverage.to_s
+    " ONE_COVERAGE_BASES " + @numBases1Coverage.to_s + 
+    " PER_ONE_COVERAGE_BASES " + @perBases1Coverage.to_s +
+    " FOUR_COVERAGE_BASES " + @numBases4Coverage.to_s + 
+    " PER_FOUR_COVERAGE_BASES " + @perBases4Coverage.to_s +
+    " TEN_COVERAGE_BASES " + @numBases10Coverage.to_s + 
+    " PER_TEN_COVERAGE_BASES " + @perBases10Coverage.to_s +
+    " TWENTY_COVERAGE_BASES " + @numBases20Coverage.to_s + 
+    " PER_TWENTY_COVERAGE_BASES " + @perBases20Coverage.to_s +
+    " FORTY_COVERAGE_BASES " + @numBases40Coverage.to_s + 
+    " PER_FORTY_COVERAGE_BASES " + @perBases40Coverage.to_s
+    return result
+  end
+=end
+
+  # Method to create a result string to upload to LIMS
+  def formatForLIMSUpload()
+    result = "TOTAL_READS " + @totalReadsProduced.to_s +
+    " DUPLICATE_READS " + @numDuplicateReads.to_s +
+    " PERCENT_DUPLICATE_READS " + @perDuplicateReads.to_s +
+    " TOTAL_ALIGNED_READS " + @numAlignedReads.to_s +
+    " PERCENT_ALIGNED_READS " + @perAlignedReads.to_s +
+    " READS_PAIRED " + @numReadsPaired.to_s + 
+    " READS_PAIRED_MAPPED_MATES " + @numReadAndMatePaired.to_s +
+    " BUFFER_ALIGNED_READS " + @numBufferAlignedReads.to_s +
+    " PERCENT_BUFFER_ALIGNED_READS " + @perBufferAlignedReads.to_s +
+    " TARGET_ALIGNED_READS " + @numTargetAlignedReads.to_s +
+    " PERCENT_TARGET_ALIGNED_READS " + @perTargetAlignedReads.to_s +
+    " AVERAGE_COVERAGE " + @avgCoverage.to_s +
+    " READS_ON_TARGET_OR_BUFFER " + @numReadsTargetBuffer.to_s +
+    " PERCENT_READS_ON_TARGET_OR_BUFFER " + @perReadsTargetBuffer.to_s +
+    " TOTAL_EXPECTED_ALIGNED_READS " + @totalExpAlignedReads.to_s +
+    " TOTAL_CALC_ALIGNED_READS " + @totalCalcAlignedReads.to_s + 
+    " TARGETS_HIT " + @numTargetsHit.to_s + " PERCENT_TARGETS_HIT " + @perTargetsHit.to_s +
+    " TARGET_BUFFERS_HIT " + @numTargetBuffersHit.to_s +
+    " PER_TARGET_BUFFERS_HITS " + @perTargetBuffersHit.to_s +
+    " TOTAL_TARGETS " + @numTotalTargets.to_s +
+    " HIGH_COVERAGE_NON_TARGET_HITS " + @numNonTarget.to_s +
+    " BASES_ON_TARGET " + @numTargetedBases.to_s + " BASES_ON_BUFFER " + @numBufferBases.to_s +
+    " ONE_COVERAGE_BASES " + @numBases1Coverage.to_s +
+    " PER_ONE_COVERAGE_BASES " + @perBases1Coverage.to_s +
+    " FOUR_COVERAGE_BASES " + @numBases4Coverage.to_s +
+    " PER_FOUR_COVERAGE_BASES " + @perBases4Coverage.to_s +
+    " TEN_COVERAGE_BASES " + @numBases10Coverage.to_s +
+    " PER_TEN_COVERAGE_BASES " + @perBases10Coverage.to_s +
+    " TWENTY_COVERAGE_BASES " + @numBases20Coverage.to_s +
+    " PER_TWENTY_COVERAGE_BASES " + @perBases20Coverage.to_s +
+    " FORTY_COVERAGE_BASES " + @numBases40Coverage.to_s +
+    " PER_FORTY_COVERAGE_BASES " + @perBases40Coverage.to_s
     return result
   end
 
   private
   # Initialize all variables to default value
   def initializeResultVariables()
+    @totalReadsProduced    = 0 # Total reads
+    @numDuplicateReads     = 0 # Num. duplicate reads
+    @perDuplicateReads     = 0 # Percentage of duplicate reads
+    @numAlignedReads       = 0 # Num. of reads aligned
+    @perAlignedReads       = 0 # Percentage of reads aligned
+    @numReadsPaired        = 0 # Total pairs of reads
+    @numReadAndMatePaired  = 0 # Read pairs with mapped mates
     @numBufferAlignedReads = 0 # Num. reads aligned on buffer
     @perBufferAlignedReads = 0 # Percentage of reads aligned on buffer
     @numTargetAlignedReads = 0 # Num. reads aligned on target
     @perTargetAlignedReads = 0 # Percentage of reads aligned on target
+    @avgCoverage           = 0 # Average coverage
+    @numReadsTargetBuffer  = 0 # Num. reads hitting target or buffer
+    @perReadsTargetBuffer  = 0 # Percentage of reads hitting target or buffer
+    @totalExpAlignedReads  = 0 # Total expected number of aligned reads
+    @totalCalcAlignedReads = 0 # Total calculated number of aligned reads 
     @numTargetsHit         = 0 # Number of targets hit
     @perTargetsHit         = 0 # Percentage of targets hit
     @numTargetBuffersHit   = 0 # Num. target buffers hit
@@ -267,6 +353,13 @@ class CaptureStatsResults
     output.gsub!(/%\)$/, "")
     return output
   end 
+
+  # Method to remove parentheses around 
+  def removeParentheses(input)
+    output = input.gsub(/\(/, "")
+    output.gsub!(/\)/, "")
+    return output
+  end
 end
 
 bamFile    = ARGV[0]
