e870f5809ddc5cede3f2dcfdf24dac2164569d90
Nirav Shah <niravhgsc@gmail.com>
2011-03-02 15:06:21 -0600

    Remove redundant code to email Summary.htm as attachment and instead modified postrun to use EmailHelper to send that email.

--- bin/postrun.rb
+++ bin/postrun.rb
@@ -3,16 +3,33 @@ $:.unshift File.join(File.dirname(__FILE__), ".", "..", "lib")
 
 require 'Scheduler'
 require 'PipelineHelper'
+require 'EmailHelper'
 
-# This is a new post run script that does not copy files to the mini analysis
-# directory. It maps the sequence files using BWA aligner.
+# This postrun is automatically called by CASAVA after sequences are generated.
+# We upload the Summary.htm to LIMS, send it via email to recepients, start the
+# uniqueness analysis and start BWA alignment.
+
+# Method to send the Summary.htm as an email attachment
+def emailSummaryHTML(fcBarcode)
+  attachFile = "Summary.htm"
+  currDir    = `pwd`
+  bodyText   = "The path to the summary file is " + currDir
+  subject    = "Illumina Lane Summary : " + fcBarcode.to_s
+  emailFrom  = "sol-pipe@bcm.edu"  
+  obj        = EmailHelper.new()
+  emailTo    = obj.getResultRecepientEmailList()
+
+  puts "Sending Summary.htm via email..."
+  obj.sendEmailWithAttachment(emailFrom, emailTo, subject, bodyText, attachFile)
+  puts "Completed"
+end
 
 helper = PipelineHelper.new()
 fcName = helper.findFCName()
 puts "Flowcell Name : " + fcName
 lanes  = helper.findAnalysisLaneNumbers()
 puts "Analysis Lane Number : " + lanes.to_s
-fcBarCode = fcName + "_" + lanes.to_s
+fcBarCode = fcName + "-" + lanes.to_s
 
 #Upload HTML summary to LIMS
 uploadLIMSHTMLCmd = "ruby /stornext/snfs5/next-gen/Illumina/ipipe/bin/upload_LIMS_summary.rb"
@@ -20,9 +37,7 @@ output = `#{uploadLIMSHTMLCmd}`
 puts output
 
 # Email the Summary.htm file
-emailHTMLsummaryCmd = "sh /stornext/snfs5/next-gen/Illumina/ipipe/bin/email_analysis_summary.sh"
-output = `#{emailHTMLsummaryCmd}`
-puts output
+emailSummaryHTML(fcBarCode)
 
 # Map sequence files using BWA
 bwaCmd = "ruby /stornext/snfs5/next-gen/Illumina/ipipe/bin/bwa_bam.rb"
