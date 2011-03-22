22d2e7668b2a843105f16b3c6d0f5f712b648fc4
Nirav Shah <niravhgsc@gmail.com>
2011-03-22 11:19:34 -0500

    Removed Plot.java and made it part of a common class. Changed GenerateJAR to compile Plot.java in common directory and include it in the JAR.

--- java/SlxUniqueness/GenerateJAR.sh
+++ java/SlxUniqueness/GenerateJAR.sh
@@ -5,8 +5,12 @@ manifestFile=SlxUniqueness_Manifest.txt
 rm *.class
 rm $jarFileName
 echo "Compiling Java Classes"
-javac *.java
+javac *.java ../common/*.java
+
 echo "Generating Manifest File"
 echo -e "Main-Class: Driver\n" > $manifestFile 
 echo "Generating Jar"
+
+cp ../common/*.class .
+
 jar cvfm $jarFileName $manifestFile *.class
